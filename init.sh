b3396c502a6f37893ba1a57cf26774355217bea5
codesenju <codesenju@gmail.com>
2020-07-13 13:04:45 +0200

    added comments to init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+#Set up a service using Docker Swarm
 docker swarm init --advertise-addr $Network_Addr
 docker stack deploy --compose-file docker-compose.yml $Service_Name
 docker service ls
